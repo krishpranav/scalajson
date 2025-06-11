eb6efed5ee29e36bc6b62bf57965c55317f949c5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-06-11 18:00:24 +0530

    scalajson: Exception

--- src/main/scala/Exception.scala
+++ src/main/scala/Exception.scala
@@ -1,11 +1,26 @@
-package com.scalajson
+package com.json
 
-object Exception {
-  class SystemException(val kind: String, val Info: Json) extends Exception {
-    override def toString(): String = kind + ":" + Compact(ino)
+import com.json.JsonOps._
+
+object Exceptions {
+
+  class SystemException(val kind: String, val info: Json) extends Exception {
+    override def toString(): String = kind + ":" + Compact(info)
   }
 
-  class JsonParseException(val msg: String, val input: String) {
-    
+  class JsonParseException(val msg: String, val input: String, val line: Int, val char: Int)
+    extends SystemException("JsonParse", JsonObject("msg" -> msg, "line" -> line, "char" -> char, "input" -> input)) {
+
+    def shortString() = {
+      "[" + jgetInt(info, "line") + "," + jgetInt(info, "char") + "] " + jgetString(info, "msg")
+
+    }
+
+    override def toString() = {
+      shortString + " (" + jgetString(info, "input") + ")"
+    }
   }
+  
+  case class MappingException(val msg: String, val path: String = "") extends SystemException("Mapping", JsonObject("msg" -> msg, "path" -> path))
+
 }
